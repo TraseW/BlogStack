766347baea3ae9fb8cafa62a3e8d25f7151965ab
Trase Westbrook <trasewestbrook@gmail.com>
2016-02-29 16:15:50 -0600

    Updated admin panel

--- blogengine.py
+++ blogengine.py
@@ -1,3 +1,5 @@
+
+
 from flask import Flask, render_template, url_for, request, redirect, flash
 from flask import session
 import os, datetime
--- template.py
+++ template.py
@@ -1,4 +1,5 @@
 from blogengine import pageLocation
+from example import main
 import pickle, os, ast
 
 def createPage(info):
@@ -16,8 +17,3 @@ def getPage(name):
         out = pickle.load(f)
         print(out[name]['content'][1]['src'])
         return out[name]
-
-
-
-
-
