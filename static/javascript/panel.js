1cbc5427f74b3ecd043b3b611578363348e4d48a
Trase Westbrook <trasewestbrook@gmail.com>
2016-03-03 18:33:25 -0600

    ajax page creation - works

--- static/javascript/panel.js
+++ static/javascript/panel.js
@@ -27,15 +27,71 @@ var pageItems = new Vue({
     removeItem: function(index) {
       this.content.splice(index, 1);
     },
+    addMeta: function() {
+      this.meta.push({
+        open: false,
+        title: '',
+        value: ''
+      });
+    },
+    removeMeta: function(index) {
+      this.meta.splice(index, 1);
+    },
     dropdown: function(index) {
       this.content[index].open = !this.content[index].open;
       if (this.content[index].open) {
         autosize($('textarea'));
       }
     },
+    dropdownMeta: function(index) {
+      this.meta[index].open = !this.meta[index].open;
+      if (this.meta[index].open) {
+        autosize($('textarea'));
+      }
+    },
     submit: function() {
       console.log('test');
       $.post('/admin/create', JSON.stringify(this.$data));
+      window.location = "/admin";
+    }
+  }
+
+});
+
+var metaItems = new Vue({
+
+  el: '#meta',
+
+  data: {
+    meta: []
+  },
+
+  created: function() {
+    this.addMeta();
+  },
+
+  methods: {
+    addMeta: function() {
+      this.meta.push({
+        open: false,
+        title: '',
+        value: ''
+      });
+    },
+    removeMeta: function(index) {
+      this.meta.splice(index, 1);
+    },
+    dropdownMeta: function(index) {
+      this.meta[index].open = !this.meta[index].open;
+      if (this.meta[index].open) {
+        autosize($('textarea'));
+      }
+    },
+    submit: function() {
+      console.log('test');
+      pageItems.meta = this.meta;
+      $.post('/admin/create', JSON.stringify(pageItems.$data));
+      window.location = "/admin";
     }
   }
 
